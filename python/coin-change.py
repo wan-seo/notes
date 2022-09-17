6ea90bd2962d228c4795320cfedef5de3d2c9335
wan-seo <wseokim@gmail.com>
2022-09-17 21:37:22 +0900

    format: apply flake8 and black

--- python/coin-change.py
+++ python/coin-change.py
@@ -7,9 +7,9 @@ class Solution:
         arr = [MAX_VAL] * (amount + 1)
         arr[0] = 0
         for coin in coins:
-            for i in range(coin, amount+1):
-                if arr[i-coin] != MAX_VAL:
-                    arr[i] = min(arr[i], arr[i-coin] + 1)
+            for i in range(coin, amount + 1):
+                if arr[i - coin] != MAX_VAL:
+                    arr[i] = min(arr[i], arr[i - coin] + 1)
 
         if arr[amount] == MAX_VAL:
             return -1
@@ -17,7 +17,7 @@ class Solution:
         return arr[amount]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # test values
     coins = [1, 2, 5]
     amount = 11
